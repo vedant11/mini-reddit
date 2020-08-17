19709c60b86b64e50f9290ca107ff5641ec54ff8
Vedant Panchal <pvipulkumar@me.iitr.ac.in>
2020-08-17 17:30:47 +0530

    added new post parameter in models

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -7,6 +7,7 @@ from django.utils import timezone
 class Post(models.Model):
     post_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
+    post_message = models.TextField(default="this is an empty post body")
 
     def __str__(self):
         return self.post_text
@@ -25,3 +26,12 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+
+"""
+DROP SCHEMA public CASCADE;
+CREATE SCHEMA public;
+
+GRANT ALL ON SCHEMA public TO postgres;
+GRANT ALL ON SCHEMA public TO public;
+"""
